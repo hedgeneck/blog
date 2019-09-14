c6e7badbf4e9476dd600cd033d29a0338ad0fa79
Gustavo Comerlatto <gustcomer@gmail.com>
2019-09-14 15:14:13 -0300

    Aula 5

---
+++ blog/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dbfd46992a739efa9b8f10ebc97338b4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from blog import routes
